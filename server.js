15028107280cc5759858cf0dfe5bb3db1464b5bc
Nick Algarra <algarran24@gmail.com>
2020-05-02 10:22:49 -0500

    added to script files for controllers

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 const express = require("express");
+var exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const routes = require("./controllers/burgers_controllers");
+
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
